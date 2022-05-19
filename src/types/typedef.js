81ce7d1de38df49896d2dde2d80a054d6a70cda3
Edinho Monteiro <bruno.monteirodg@gmail.com>
2022-05-19 13:57:13 -0300

    chore: add typedefs and editor settings

---
+++ src/types/typedef.js
@@ -0,0 +1,18 @@
+/**
+ * @typedef RichTextNode
+ * @property { string } nodeType
+ * @property { string } value
+ * @property { Array<MarkType> } marks
+ * @property { RichTextNodeData } data
+ * @property { Array<RichTextNode> } content
+ */
+
+/**
+ * @typedef MarkType
+ * @property { string } type
+ */
+
+/**
+ * @typedef RichTextNodeData
+ * @property { string } [uri]
+ */
